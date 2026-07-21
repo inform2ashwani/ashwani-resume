ec0ba67ec67b9988ba9cec1562f7c47b7d3e88b3
Ashwani Kumar <inform2ashwani@gmail.com>
2026-07-21 21:24:36 +0530

    changes

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,59 +1,39 @@
-import { Inject, Injectable } from "@angular/core";
-import { DOCUMENT } from "@angular/common";
+import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 
 import { IExperience } from "../experience/experience-interfaces";
 import { IAbout } from "../about/about-interfaces";
 import { IPost } from "../posts/posts-interfaces";
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class DataService {
 
-    private readonly baseUrl: string;
-    
-    constructor(
-        private http: HttpClient,
-        @Inject(DOCUMENT) private document: Document
-    ) {
-        this.baseUrl = new URL("assets/data/", this.document.baseURI).toString();
-    }
+  private readonly baseUrl = '/assets/data/';
 
-    getExperiences() : Observable<IExperience[]> {
-        return this.http.get<IExperience[]>(this.getDataUrl("experiences.json"))
-            .pipe(
-                catchError(this.handleError)
-            );
-    }
-    
-    getAbout() : Observable<IAbout> {
-      return this.http.get<IAbout>(this.getDataUrl("about.json"))
-          .pipe(
-              catchError(this.handleError)
-          );
-    }
+  constructor(private http: HttpClient) {}
 
-    getPosts() : Observable<IPost[]> {
-        return this.http.get<IPost[]>(this.getDataUrl("posts.json"))
-            .pipe(
-                catchError(this.handleError)
-            );
-    }
+  getExperiences(): Observable<IExperience[]> {
+    return this.http.get<IExperience[]>(`${this.baseUrl}experiences.json`)
+      .pipe(catchError(this.handleError));
+  }
 
-    private getDataUrl(fileName: string): string {
-        return new URL(`assets/data/${fileName}`, this.document.baseURI).toString();
-    }
+  getAbout(): Observable<IAbout> {
+    return this.http.get<IAbout>(`${this.baseUrl}about.json`)
+      .pipe(catchError(this.handleError));
+  }
 
-    private handleError(error: any) {
-      console.error("server error:", error);
-      if (error.error instanceof Error) {
-          const errMessage = error.error.message;
-          return Observable.throw(errMessage);
-          // Use the following instead if using lite-server
-          // return Observable.throw(err.text() || "backend server error");
-      }
-      return Observable.throw(error || "Node.js server error");
-    }
+  getPosts(): Observable<IPost[]> {
+    return this.http.get<IPost[]>(`${this.baseUrl}posts.json`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any) {
+    console.error("server error:", error);
+    const errMessage = error.error?.message || error.message || "backend server error";
+    return throwError(() => errMessage);
+  }
 }
