a94342ccc225a4d9f2b3f66530f88ac531a665d4
Ashwani Kumar <inform2ashwani@gmail.com>
2026-07-21 19:44:35 +0530

    changs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,35 +2,25 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ResumeComponent } from "./resume/resume.component";
 
-// Any URL apart from the root domain is going to be rendered as "page-not-found".
 const routes: Routes = [
-  { path: "", component: ResumeComponent},
-  { path: "about", redirectTo: "/#about"},
-  { path: "experience", redirectTo: "/#experience"},
-  { path: "experiences", redirectTo: "/#experience"},
-  { path: "posts", redirectTo: "/#posts"},
-  { path: "contact", redirectTo: "/#contact"},
+  { path: "", component: ResumeComponent },   // root shows English resume
+  { path: "en", component: ResumeComponent }, // optional explicit /en route
+  { path: "about", redirectTo: "/#about" },
+  { path: "experience", redirectTo: "/#experience" },
+  { path: "experiences", redirectTo: "/#experience" },
+  { path: "posts", redirectTo: "/#posts" },
+  { path: "contact", redirectTo: "/#contact" },
   { path: "**", redirectTo: "/page-not-found" }
 ];
 
+
 @NgModule({
-  imports: [ RouterModule.forRoot(
-    routes,
-    {
-      // In order to get anchor / fragment scrolling to work at all, we need to
-      // enable it on the router.
+  imports: [
+    RouterModule.forRoot(routes, {
       anchorScrolling: "enabled",
-
-      // Once the above is enabled, the fragment link will only work on the
-      // first click. This is because, by default, the Router ignores requests
-      // to navigate to the SAME URL that is currently rendered. Unfortunately,
-      // the fragment scrolling is powered by Navigation Events. As such, we
-      // have to tell the Router to re-trigger the Navigation Events even if we
-      // are navigating to the same URL.
       onSameUrlNavigation: "reload"
-    }
-  )],
-  exports: [ RouterModule ]
+    })
+  ],
+  exports: [RouterModule]
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
