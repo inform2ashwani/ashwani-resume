fc477ffe39c20eef8ef3f78ca7aac75f4b852ce3
Ashwani Kumar <inform2ashwani@gmail.com>
2026-07-21 20:44:02 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { registerLocaleData } from "@angular/common";
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -42,7 +43,8 @@ registerLocaleData(localePt, "pt-BR", localePtExtra);
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule,
     AngularFireAnalyticsModule,
-    HammerModule
+    HammerModule,
+    RouterModule
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ],
